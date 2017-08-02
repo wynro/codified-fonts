70378ccbe5b73669b028c2e8d56ff09ca6a95187
Guillermo Robles <guillerobles1995@gmail.com>
2017-08-02 23:28:15 +0200

    Fairly big rewriting

--- atbash/generate-font.py
+++ atbash/generate-font.py
@@ -7,14 +7,8 @@ Based in the script by
 Author: Guillermo Robles
 """
 
-# Load module
 import fontforge
-import os
-
-
-def transformate(character):
-    # Receive a character, return the codified character
-    return chr((ord('z') - ord(character))+ord('a'))
+from os import remove
 
 
 FONT_NAME = "Codified-atbash"
@@ -23,53 +17,86 @@ FULLNAME = FONT_NAME
 VERSION = "0.1"
 COPYRIGHT = "Font based in Carlito, by tyPoland Lukasz Dziedzic. Licensed under the SIL Open Font License, Version 1.1"
 
-TRANSFORMATION = {}
-for i in range(ord('a'), ord('a') + 26):
-    TRANSFORMATION[chr(i)] = transformate(chr(i))
 
-# Get the font to be modified (Current font)
-myFont = fontforge.open("../original/Carlito-Regular.ttf")
+def transformate(character):
+    # Receive a character, return the codified character
+    return chr((ord('z') - ord(character)) + ord('a'))
 
-# Create a font to use as auxiliar variable
-temporal = fontforge.font()
 
-# Change the order of the letters
-for i in TRANSFORMATION:
-    # Lowercase glyph
-    myFont.selection.select(i)
-    myFont.copy()
-    temporal.selection.select(TRANSFORMATION[i])
-    temporal.paste()
-    # Uppercase glyph
-    myFont.selection.select(i.upper())
-    myFont.copy()
-    temporal.selection.select(TRANSFORMATION[i].upper())
-    temporal.paste()
+# End of editable values
 
-# Copy back the letters in the auxiliar font to the original font.
-temporal.selection.select(("ranges", None), "a", "z")
-myFont.selection.select(("ranges", None), "a", "z")
-temporal.copy()
-myFont.paste()
-temporal.selection.select(("ranges", None), "A", "Z")
-myFont.selection.select(("ranges", None), "A", "Z")
-temporal.copy()
-myFont.paste()
 
-# Edit font metadata
-myFont.fontname = FONT_NAME
-myFont.familyname = FAMILY_NAME
-myFont.fullname = FULLNAME
-myFont.version = VERSION
-myFont.copyright = COPYRIGHT
+def generate_dictionary(transformate):
+    transformation = {}
+    for i in range(ord('a'), ord('a') + 26):
+        transformation[chr(i)] = transformate(chr(i))
+    return transformation
 
-temporal.close()
 
-myFont.generate(FONT_NAME + ".sfd")
-myFont.close()
-myFont = fontforge.open(FONT_NAME + ".sfd")
-myFont.generate(FONT_NAME + ".ttf")
-myFont.close()
+def generate_font(from_font,
+                  transformation,
+                  font_name,
+                  family_name,
+                  fullname,
+                  version,
+                  copyright):
+    # Get the font to be modified (Current font)
+    myFont = fontforge.open(from_font)
 
-os.remove(FONT_NAME + ".sfd")
-os.remove(FONT_NAME + ".afm")
+    # Create a font to use as auxiliar variable
+    temporal = fontforge.font()
+
+    # Change the order of the letters
+    for i in transformation:
+        # Lowercase glyph
+        myFont.selection.select(i)
+        myFont.copy()
+        temporal.selection.select(transformation[i])
+        temporal.paste()
+        # Uppercase glyphs
+        myFont.selection.select(i.upper())
+        myFont.copy()
+        temporal.selection.select(transformation[i].upper())
+        temporal.paste()
+
+    # Copy back the letters in the auxiliar font to the original font.
+    temporal.selection.select(("ranges", None), "a", "z")
+    myFont.selection.select(("ranges", None), "a", "z")
+    temporal.copy()
+    myFont.paste()
+    temporal.selection.select(("ranges", None), "A", "Z")
+    myFont.selection.select(("ranges", None), "A", "Z")
+    temporal.copy()
+    myFont.paste()
+
+    # Edit font metadata
+    myFont.fontname = font_name
+    myFont.familyname = family_name
+    myFont.fullname = fullname
+    myFont.version = version
+    myFont.copyright = copyright
+
+    temporal.close()
+
+    myFont.generate(font_name + ".sfd")
+    myFont.close()
+    myFont = fontforge.open(font_name + ".sfd")
+    myFont.generate(font_name + ".ttf")
+    myFont.close()
+
+    remove(font_name + ".sfd")
+    remove(font_name + ".afm")
+
+
+def main():
+    generate_font("../original/Carlito-Regular.ttf",
+                  generate_dictionary(transformate),
+                  FONT_NAME,
+                  FAMILY_NAME,
+                  FULLNAME,
+                  VERSION,
+                  COPYRIGHT)
+
+
+if __name__ == "__main__":
+    main()
