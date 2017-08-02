40b72043b0d0233868f16b8afa2eb6df56b6da32
Guillermo Robles <guillerobles1995@gmail.com>
2017-08-03 00:09:04 +0200

    Add licensing

---
+++ LICENSING.md
@@ -0,0 +1,9 @@
+# Fonts
+
+All the fonts in this repository created by me (that's it, all the fonts that are not in `original/`, or whose glyphs are not based in **Carlito**) are licensed under the [SIL Open Font License](http://scripts.sil.org/OFL), unless explicitly stated. I don't apply a license to the fonts derived from **Carlito**, because they are just transpositions of **Carlito** itself.
+
+The fonts currently derived from **Carlito** are `atbash` and `caesar`.
+
+# Code
+
+All the code in this repository is licensed under the GPLv2 license, unless explicitly stated.
--- caesar/generate-font.py
+++ caesar/generate-font.py
@@ -5,6 +5,23 @@ Generate a codified font from another one.
 Based in the script by jfmdev (https://github.com/jfmdev/TuringFonts)
 
 Author: Guillermo Robles
+License: GPLv2
+
+    Copyright (C) 2017  Guillermo Robles
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
 import fontforge
